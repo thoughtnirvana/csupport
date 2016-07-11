84cd3c9f607e31762a2690298446ff4527d0fb65
Ujjawal <ujjawal@thoughtnirvana.com>
2016-07-11 14:51:33 +0530

    token auth for api

--- csupportapi/config/routes.rb
+++ csupportapi/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
