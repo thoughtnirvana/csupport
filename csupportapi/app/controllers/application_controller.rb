3513b6ea37950eada86da393010c4d0e999bf6e7
Ujjawal <ujjawal@thoughtnirvana.com>
2016-07-11 21:08:23 +0530

    sanitize role_mask for devise

--- csupportapi/app/controllers/application_controller.rb
+++ csupportapi/app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :roles_mask])
   end
 end
